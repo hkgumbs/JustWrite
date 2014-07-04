83bd64bb3f1e5590f321b23b9e683e746dccb4de
hkgumbs <h.kofigumbs@gmail.com>
2014-07-04 14:27:54 -0700

    uncoupled the app

--- src/me/hkgumbs/just_write/ContentFragment.java
+++ src/me/hkgumbs/just_write/ContentFragment.java
@@ -1,15 +1,10 @@
 package me.hkgumbs.just_write;
 
-import com.squareup.seismic.ShakeDetector;
-
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.TypedValue;
@@ -17,23 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ScrollView;
 
 public class ContentFragment extends Fragment {
 
-    private int position;
-
     private SharedPreferences sp;
-    private Boolean dark;
-    private ShakeDetector sd;
-
-    private FrameLayout fl;
-    private ScrollView sv;
     private EditText content;
-
-    private AlertDialog ad;
-    private LayoutInflater in;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,18 +25,14 @@ public class ContentFragment extends Fragment {
 
 	final View frame = inflater.inflate(R.layout.fragment_content,
 		container, false);
-	position = getArguments().getInt("position");
 
 	// BOOTSTRAP
 	sp = getActivity().getPreferences(Context.MODE_PRIVATE);
-	fl = (FrameLayout) frame.findViewById(R.id.root);
-	sv = (ScrollView) frame.findViewById(R.id.containter);
 	content = (EditText) frame.findViewById(R.id.text);
-	in = getActivity().getLayoutInflater();
 
 	// INIT CONTENT
 	Typeface typeFace = Typeface.createFromAsset(getActivity().getAssets(),
-		"fonts/RobotoSlab-Thin.ttf");
+		sp.getString("FONT", "fonts/RobotoSlab-Thin.ttf"));
 	content.setTypeface(typeFace);
 	content.setText(sp.getString("CONTENT", ""));
 	content.setTextSize(TypedValue.COMPLEX_UNIT_SP,
@@ -78,27 +57,6 @@ public class ContentFragment extends Fragment {
 	});
 
 	return frame;
-    }
-
-    static class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-	public ScreenSlidePagerAdapter(FragmentManager fm) {
-	    super(fm);
-	}
-
-	@Override
-	public Fragment getItem(int position) {
-	    Fragment fragment = new ContentFragment();
-	    Bundle arg = new Bundle();
-	    arg.putInt("position", position);
-	    fragment.setArguments(arg);
-	    return fragment;
-	}
-
-	@Override
-	public int getCount() {
-	    return 5;
-	}
-
     }
 
 }
--- src/me/hkgumbs/just_write/MainActivity.java
+++ src/me/hkgumbs/just_write/MainActivity.java
@@ -1,56 +1,20 @@
 package me.hkgumbs.just_write;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.sql.Timestamp;
-
-import me.hkgumbs.just_write.ContentFragment.ScreenSlidePagerAdapter;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.TypedValue;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 
 import com.squareup.seismic.ShakeDetector;
 
-import android.app.AlertDialog;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.DialogInterface.OnShowListener;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.hardware.SensorManager;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements
 	ShakeDetector.Listener {
@@ -58,20 +22,10 @@ public class MainActivity extends FragmentActivity implements
     private SharedPreferences sp;
     private ShakeDetector sd;
 
-    private FrameLayout fl;
-    private ScrollView sv;
-    private EditText content;
-
-    private AlertDialog ad;
-    private LayoutInflater in;
-
-    ScreenSlidePagerAdapter pagerAdapter;
+    MyMenu menu;
+    ActionHandler handler;
+    MyFragmentAdapter pagerAdapter;
     ViewPager pager;
-
-    private final static int NOTIFICATION_ID = 88;
-    private final static int MIN_FONT_SIZE = 12;
-    private static String[] commands;
-    private static int[] drawables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,10 +33,13 @@ public class MainActivity extends FragmentActivity implements
 	requestWindowFeature(Window.FEATURE_NO_TITLE);
 	setContentView(R.layout.activity_home);
 
-	pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+	sp = getPreferences(Context.MODE_PRIVATE);
+	pagerAdapter = new MyFragmentAdapter(getSupportFragmentManager());
 	pager = (ViewPager) findViewById(R.id.pager);
 	pager.setAdapter(pagerAdapter);
 
+	handler = new ActionHandler(this);
+	menu = new MyMenu(this, handler);
     }
 
     @Override
@@ -111,219 +68,36 @@ public class MainActivity extends FragmentActivity implements
 
     public void hearShake() {
 	// shake detector implemented in square-seismic-1.0.0.jar
-
-	if (!ad.isShowing()) {
-	    ad.show();
+	int position = pager.getCurrentItem();
+	View root = pager.getChildAt(position);
+	if (menu.open(position, root)) {
+	    // hide keyboard
+	    InputMethodManager imm = (InputMethodManager) this
+		    .getSystemService(Context.INPUT_METHOD_SERVICE);
+	    imm.hideSoftInputFromWindow(pager.getWindowToken(), 0);
 	}
     }
 
-    // EDIT FONT SIZE
-    private void showBar() {
+    class MyFragmentAdapter extends FragmentStatePagerAdapter {
 
-	View layout = in.inflate(R.layout.dialog_font,
-		(ViewGroup) findViewById(R.layout.activity_home));
-	new AlertDialog.Builder(this).setView(layout)
-		.setPositiveButton("OK", new OnClickListener() {
+	public MyFragmentAdapter(FragmentManager fm) {
+	    super(fm);
+	}
 
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-			sp.edit()
-				.putFloat("FONT_SIZE",
-					toSP(content.getTextSize())).apply();
-		    }
+	@Override
+	public ContentFragment getItem(int position) {
+	    ContentFragment fragment = new ContentFragment();
+	    Bundle arg = new Bundle();
+	    arg.putInt("position", position);
+	    fragment.setArguments(arg);
+	    return fragment;
+	}
 
-		}).setNegativeButton("Cancel", new OnClickListener() {
-
-		    @Override
-		    public void onClick(DialogInterface arg0, int arg1) {
-			content.setTextSize(TypedValue.COMPLEX_UNIT_SP,
-				sp.getFloat("FONT_SIZE", 50));
-		    }
-
-		}).create().show();
-
-	SeekBar sb = (SeekBar) layout.findViewById(R.id.font);
-	sb.setMax(100 - MIN_FONT_SIZE);
-	sb.setProgress(toSP(content.getTextSize()) - MIN_FONT_SIZE);
-	sb.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-
-	    @Override
-	    public void onProgressChanged(SeekBar seekBar, int progress,
-		    boolean fromUser) {
-		content.setTextSize(TypedValue.COMPLEX_UNIT_SP, progress
-			+ MIN_FONT_SIZE);
-	    }
-
-	    @Override
-	    public void onStartTrackingTouch(SeekBar seekBar) {
-	    }
-
-	    @Override
-	    public void onStopTrackingTouch(SeekBar seekBar) {
-	    }
-
-	});
+	@Override
+	public int getCount() {
+	    return sp.getInt("pages", 1);
+	}
 
     }
 
-    // SAVE AS IMAGE
-    private void capture() {
-
-	// CREATE BITMAP FROM SCREEN CAPTURE
-	content.setCursorVisible(false);
-	fl.setDrawingCacheEnabled(true);
-	Bitmap bitmap = Bitmap.createBitmap(fl.getDrawingCache());
-	fl.setDrawingCacheEnabled(false);
-	content.setCursorVisible(true);
-
-	new AsyncTask<Bitmap, Void, Boolean>() {
-	    String name;
-	    String root;
-	    File file;
-
-	    @Override
-	    protected Boolean doInBackground(Bitmap... b) {
-
-		// IMAGE NAMING
-		name = new Timestamp(new java.util.Date().getTime()).toString()
-			+ ".jpg";
-		root = Environment.getExternalStorageDirectory().toString()
-			+ "/Just Write/";
-		File dir = new File(root);
-		dir.mkdirs();
-		file = new File(dir, name);
-		if (file.exists())
-		    file.delete();
-
-		// SAVE TO DEVICE
-		try {
-		    FileOutputStream out = new FileOutputStream(file);
-		    b[0].compress(Bitmap.CompressFormat.JPEG, 90, out);
-		    out.flush();
-		    out.close();
-
-		} catch (Exception e) {
-		    e.printStackTrace();
-		    return false;
-		}
-
-		return true;
-	    }
-
-	    @Override
-	    protected void onPostExecute(Boolean result) {
-
-		// ERROR CASE
-		if (!result) {
-		    Toast.makeText(MainActivity.this, "Save failed",
-			    Toast.LENGTH_SHORT).show();
-		    return;
-		}
-
-		// REFRESH GALLERY
-		ContentValues values = new ContentValues();
-		values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
-		values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-		getContentResolver().insert(
-			MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-		// CREATE NOTIFICATION
-		Intent i = new Intent();
-		i.setAction(Intent.ACTION_VIEW);
-		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		i.setDataAndType(Uri.parse("file://" + root + name), "image/*");
-
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(
-			MainActivity.this)
-			.setContentTitle(getString(R.string.notification_title))
-			.setContentText(getString(R.string.notification_desc))
-			.setSmallIcon(R.drawable.notification_icon)
-			.setAutoCancel(true)
-			.setTicker(getString(R.string.notification_title))
-			.setLargeIcon(
-				BitmapFactory.decodeResource(getResources(),
-					R.drawable.camera_icon))
-			.setContentIntent(
-				PendingIntent.getActivity(MainActivity.this, 0,
-					i, PendingIntent.FLAG_ONE_SHOT));
-
-		NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		nm.cancel(NOTIFICATION_ID); // remove old notification
-		nm.notify(NOTIFICATION_ID, builder.build());
-
-	    }
-
-	}.execute(bitmap);
-
-    }
-
-    private int toSP(Float px) {
-	float sd = getResources().getDisplayMetrics().scaledDensity;
-	return (int) (px / sd);
-    }
-
-    private AlertDialog initMenu() {
-
-	ListView lv = (ListView) in.inflate(R.layout.dialog_options, null);
-
-	ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
-		R.layout.item_options, commands) {
-
-	    @Override
-	    public View getView(int position, View v, ViewGroup root) {
-
-		if (v == null)
-		    v = in.inflate(R.layout.item_options, null);
-
-		Drawable d = getResources().getDrawable(drawables[position]);
-		d.setBounds(0, 0, d.getIntrinsicWidth() / 2,
-			d.getIntrinsicHeight() / 2);
-
-		((TextView) v).setText(commands[position]);
-		((TextView) v).setCompoundDrawables(d, null, null, null);
-
-		return v;
-	    }
-
-	};
-	lv.setAdapter(aa);
-
-	lv.setOnItemClickListener(new OnItemClickListener() {
-	    @Override
-	    public void onItemClick(AdapterView<?> arg0, View arg1,
-		    int position, long arg3) {
-
-		ad.dismiss();
-
-		switch (position) {
-		case 0:
-		    capture();
-		    break;
-		case 1:
-		    // setTheme(true);
-		    break;
-		case 2:
-		    showBar();
-		    break;
-		}
-
-	    }
-	});
-
-	AlertDialog alert = new AlertDialog.Builder(MainActivity.this).setView(
-		lv).create();
-
-	alert.setOnShowListener(new OnShowListener() {
-
-	    @Override
-	    public void onShow(DialogInterface dialog) {
-		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(content.getWindowToken(), 0);
-
-	    }
-
-	});
-
-	return alert;
-    }
 }
